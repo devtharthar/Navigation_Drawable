7884fe3b34df4343e1dcfacd612c5fcc5383fde6
devtharthar <dev.aungkolatt@gmail.com>
2015-12-16 15:00:57 +0630

    Initial commit

---
+++ app/src/main/java/toolbar/tharthar/dev/navigationdrawable/Tab.java
@@ -0,0 +1,16 @@
+package toolbar.tharthar.dev.navigationdrawable;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Dev_Thar_Thar on 12/2/2015.
+ */
+public class Tab extends ActionBarActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab);
+    }
+}
